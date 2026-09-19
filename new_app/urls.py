55a6a7c3f9229fd410dd67b37aafd732728d9b72
amruthtk <amruthtk777@gmail.com>
2026-09-19 11:31:38 +0530

    upto update

--- new_app/urls.py
+++ new_app/urls.py
@@ -7,4 +7,6 @@ path('abc',views.home,name='home'),
 path('menu_data',views.menu_data,name='menu_data'),
 path('',views.menu_view,name='menu_view'),
 path('new',views.new,name='new'),
+path('menu_delete/<int:id>/',views.menu_delete,name='menu_delete'),
+path('menu_update/<int:id>/',views.menu_update,name='menu_update'),
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from new_app.forms import menuForm
 from new_app.models import Menu
@@ -28,7 +28,26 @@ def new(request):
     form = menuForm()
     if request.method == 'POST':
         form = menuForm(request.POST)
-    if form.is_valid():
+        if form.is_valid():
             form.save()
 
     return render(request, 'new.html', {'form': form})
+
+def menu_delete(request,id):
+    data = Menu.objects.get(id=id)
+    print(data)
+    data.delete()
+    return redirect('menu_view')
+
+def menu_update(request,id):
+    data = Menu.objects.get(id = id)
+    print(data)
+    form = menuForm(instance=data)
+
+    if request.method == 'POST':
+        form = menuForm(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            return redirect('menu_view')
+
+    return render(request,'menu_update.html',{'data':form})
